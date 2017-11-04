c6643654a7d6bfb214c6d4477ebe70835142702c
Wellington Santos <wjsantos.wj@gmail.com>
2017-11-04 12:24:52 -0200

    Autoload metabox functions

--- wp-content/themes/ongcrescer/functions/metabox.php
+++ wp-content/themes/ongcrescer/functions/metabox.php
@@ -1,12 +1,16 @@
 <?php
   // use https://metabox.io/online-generator/ to generate new metaboxes
-  require(dirname(__FILE__) . "/metabox/home.php");
+  foreach (glob(dirname(__FILE__) . "/metabox/*.php") as $arquivo) {
+    require_once($arquivo);
+  }
 
   // loads metabox on specific pages
   global $post;
   $_post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'];
   $_page = $post ? $post : get_page($_post_id);
 
+
+  // list of pages to load metabox
   if($_page && $_page->post_name == "home"){
     add_filter( 'rwmb_meta_boxes', 'destaque_meta_box' );
   }
